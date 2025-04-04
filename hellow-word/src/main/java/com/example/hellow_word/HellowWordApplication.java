841d69bc23bede4a98dcac15fca895b36bc6f1d2
sebs456 <juansebastiancc10@gmail.com>
2025-04-03 23:56:28 -0500

    Agregado proyecto Spring Boot Hello World

---
+++ hellow-word/src/main/java/com/example/hellow_word/HellowWordApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellow_word;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellowWordApplication.class, args);
+	}
+
+}
